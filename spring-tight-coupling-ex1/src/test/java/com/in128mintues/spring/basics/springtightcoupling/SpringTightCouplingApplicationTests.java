6fc8ac6caba5ef2b39c7920471e5386ef190d3da
Kuldeep-21 <63606657+Kuldeep-21@users.noreply.github.com>
2020-04-13 23:29:25 +0530

    Add files via upload

---
+++ spring-tight-coupling-ex1/src/test/java/com/in128mintues/spring/basics/springtightcoupling/SpringTightCouplingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in128mintues.spring.basics.springtightcoupling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTightCouplingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
